6c1c3ffe5c5da5408040a03e7bc548f978d3a4b6
unknown <tirusatrapu36@gmail.com>
2025-09-24 22:01:00 +0530

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -48,7 +48,7 @@ fi
 mkdir -p /app 
 VALIDATE $? "create directory"
 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
 VALIDATE $? "Downloading catalogue appilication"
 
 cd /app 
@@ -78,7 +78,7 @@ dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "install mongodb-mongosh"
 
 
-INDEX=$(mongosh mongodb.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+INDEX=$(mongosh mongodb.tirusatrapu.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
 if [ $INDEX -le 0 ]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Load catalogue products"
