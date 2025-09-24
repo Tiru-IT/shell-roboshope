df22f6bb0b2819e0e63e390bbe84d7f55cf5546c
unknown <tirusatrapu36@gmail.com>
2025-09-24 21:03:49 +0530

    catalogue

---
+++ catalogue.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0 ]; then
+    echo "ERROR:: please run this command root user"
+    exit 1
+fi
+
+LOGS_FOLDER="/var/log/shell-roboshope"
+SCRIOT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" #/var/log/shell-roboshop/catalogue.log
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ...$R FAILER $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 ...$R SUCCESS $N" | tee -a $LOG_FILE 
+    fi
+}
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "disable nodejs"
