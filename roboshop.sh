ac65aca4ef00a50937cb8a7b85cdf846e98ba6ac
unknown <tirusatrapu36@gmail.com>
2025-09-24 15:43:49 +0530

    MongoDB INstalling

---
+++ roboshop.sh
@@ -0,0 +1,29 @@
+#!/bin bash
+
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0f3a1afbf0bbc7f0e"
+
+for instance in $@
+do
+     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro
+      --security-group-ids $SG_ID 
+      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" 
+      --query 'Instances[0].InstanceId' --output text)
+
+
+      if [ $instance -ne frontend ];then
+        IP=$(aws ec2 run-instances --image-id $INSTANCE_ID --instance-type t3.micro 
+        --security-group-ids sg-0f3a1afbf0bbc7f0e 
+        --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Test-1}]' 
+        --query 'Instances[0].PrivateIpAddress' --output text)
+    else
+        IP=$(aws ec2 run-instances --image-id $INSTANCE_ID --instance-type t3.micro 
+        --security-group-ids sg-0f3a1afbf0bbc7f0e 
+        --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Test-1}]' 
+        --query 'Instances[0].PrivateIpAddress' --output text)
+    fi
+
+    echo "$instance: $IP"
+
+done
