7d78cc5ef043a1a5f96ac3dec1dc7e255e10f78d
unknown <tirusatrapu36@gmail.com>
2025-09-24 21:45:17 +0530

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -11,6 +11,7 @@ if [ $USER_ID -ne 0 ]; then
     echo "ERROR:: please run this command root user"
     exit 1
 fi
+SCRIPT_DIR=$PWD
 
 LOGS_FOLDER="/var/log/shell-roboshope"
 SCRIOT_NAME=$( echo $0 | cut -d "." -f1 )
@@ -23,9 +24,66 @@ VALIDATE(){
     if [ $1 -ne 0 ]; then
         echo -e "$2 ...$R FAILER $N" | tee -a $LOG_FILE
     else
-        echo -e "$2 ...$R SUCCESS $N" | tee -a $LOG_FILE 
+        echo -e "$2 ...$G SUCCESS $N" | tee -a $LOG_FILE 
     fi
 }
 
 dnf module disable nodejs -y &>>$LOG_FILE
 VALIDATE $? "disable nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "enable nodejs:20"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "install nodejs"
+
+id roboshope &>>$LOG_FILE 
+if[ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+else
+    echo -e "User already exit $Y SKIPPING $N"
+fi
+
+mkdir -p /app 
+VALIDATE $? "create directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+VALIDATE $? "Downloading catalogue appilication"
+
+cd /app 
+VALIDATE $? "move to app directory"
+
+rm-rf /app/*
+VALIDATE $? "removing existing code"
+
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+VALIDATE $? "unzip the code"
+
+npm install 
+VALIDATE $? "npm install"
+
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "copy systemctl service"
+
+systemctl daemon-reload
+systemctl enable catalogue &>>$LOG_FILE
+VALIDATE $? "Enable catalogue"
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copy mongo repo"
+
+
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "install mongodb-mongosh"
+
+
+INDEX=$(mongosh mongodb.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Load catalogue products"
+else
+    echo -e "Catalogue products already loaded ... $Y SKIPPING $N"
+fi
+
+systemctl restart catalogue
+VALIDATE $? "Restarted catalogue"
