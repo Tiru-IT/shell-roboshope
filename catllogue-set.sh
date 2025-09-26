6df6dd06ddbcb3164034d7edd5d96b37c2ab0829
unknown <tirusatrapu36@gmail.com>
2025-09-26 09:58:07 +0530

    catalogue-set

--- catllogue-set.sh
+++ catllogue-set.sh
@@ -32,7 +32,7 @@ dnf install nodejs -y &>>$LOG_FILE
 echo -e "install nodjs $G success $N"
 
 
-id roboshope &>>$LOG_FILE
+id roboshop &>>$LOG_FILE
 
 if [ $? -ne 0 ]; then 
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
