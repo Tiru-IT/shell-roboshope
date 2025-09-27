fbe309c1913ffe10396b97406dff2c9c5385c21f
unknown <tirusatrapu36@gmail.com>
2025-09-27 12:56:13 +0530

    payment-set

---
+++ payment.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0 ]; then
+    echo "ERROR:: please run this command root user"
+    exit 1
+fi
+
+SCRIPT_DIR=$PWD
+MONGODB_HOST=
+SATRT_TIME=$(date +%s)
+
+LOGS_FOLDER="/var/log/shell-roboshope"
+SCRIOT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" #/var/log/shell-roboshop/catalogue.log
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ...$R FAILER $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 ...$G SUCCESS $N" | tee -a $LOG_FILE 
+    fi
+}
+#payment install....
+
+dnf install python3 gcc python3-devel -y &>>$LOG_FILE
+VALIDATE $? "install pytho3"
+id roboshop $>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+else
+    echo -e "user already exit $Y SKIPPING $N"
+fi
+
+mkdir /app 
+curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip &>>$LOG_FILE
+VALIDATE $? "download code"
+cd /app 
+rm -rf /app/* 
+VALIDATE $? "remove the code"
+
+unzip /tmp/payment.zip &>>$LOG_FILE
+VALIDATE $? "unzip the code"
+
+pip3 install -r requirements.txt &>>$LOG_FILE
+cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service &>>$LOG_FILE
+VALIDATE $? "systemctl service"
+systemctl daemon-reload
+systemctl enable payment &>>$LOG_FILE
+systemctl start payment
+VALIDATE $? "start payment"
+
+
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $SATRT_TIME ))
+echo -e "Script exicuted in $TOTAL_TIME $Y seconds $N"
